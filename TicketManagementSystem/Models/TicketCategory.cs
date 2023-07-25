7012ef958cf324795651f6095c6b3cfdbd02750f
Lucian B <lucian.bratu@stud.ubbcluj.ro>
2023-07-25 13:05:57 +0300

    Generate all the models from the database

---
+++ TicketManagementSystem/Models/TicketCategory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TicketManagementSystem.Models;
+
+public partial class TicketCategory
+{
+    public long TicketCategoryId { get; set; }
+
+    public long? EventId { get; set; }
+
+    public string? Description { get; set; }
+
+    public decimal? Price { get; set; }
+
+    public virtual Event? Event { get; set; }
+
+    public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+}
---
+++ TicketManagementSystem/Models/TicketManagementSystemDbContext.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace TicketManagementSystem.Models;
+
+public partial class TicketManagementSystemDbContext : DbContext
+{
+    public TicketManagementSystemDbContext()
+    {
+    }
+
+    public TicketManagementSystemDbContext(DbContextOptions<TicketManagementSystemDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    public virtual DbSet<Event> Events { get; set; }
+
+    public virtual DbSet<EventType> EventTypes { get; set; }
+
+    public virtual DbSet<Order> Orders { get; set; }
+
+    public virtual DbSet<TicketCategory> TicketCategories { get; set; }
+
+    public virtual DbSet<TotalNumberOfTicketsPerCategory> TotalNumberOfTicketsPerCategories { get; set; }
+
+    public virtual DbSet<Venue> Venues { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-HKAHB71;Initial Catalog=TicketManagementSystemDB;Integrated Security=True;TrustServerCertificate=True;encrypt=false;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Customer>(entity =>
+        {
+            entity.HasKey(e => e.CustomerId).HasName("PK__Customer__B611CB9D946E0F22");
+
+            entity.ToTable("Customer");
+
+            entity.HasIndex(e => e.Email, "UQ_user_email").IsUnique();
+
+            entity.Property(e => e.CustomerId).HasColumnName("customerID");
+            entity.Property(e => e.CustomerName)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("customerName");
+            entity.Property(e => e.Email)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("email");
+        });
+
+        modelBuilder.Entity<Event>(entity =>
+        {
+            entity.HasKey(e => e.EventId).HasName("PK__Event__2DC7BD69F3B6C3BD");
+
+            entity.ToTable("Event");
+
+            entity.Property(e => e.EventId).HasColumnName("eventID");
+            entity.Property(e => e.EndDate)
+                .HasColumnType("datetime")
+                .HasColumnName("endDate");
+            entity.Property(e => e.EventDescription)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("eventDescription");
+            entity.Property(e => e.EventName)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("eventName");
+            entity.Property(e => e.EventTypeId).HasColumnName("eventTypeID");
+            entity.Property(e => e.StartDate)
+                .HasColumnType("datetime")
+                .HasColumnName("startDate");
+            entity.Property(e => e.VenueId).HasColumnName("venueID");
+
+            entity.HasOne(d => d.EventType).WithMany(p => p.Events)
+                .HasForeignKey(d => d.EventTypeId)
+                .HasConstraintName("FK__Event__eventType__607D3EDD");
+
+            entity.HasOne(d => d.Venue).WithMany(p => p.Events)
+                .HasForeignKey(d => d.VenueId)
+                .HasConstraintName("FK__Event__venueID__5F891AA4");
+        });
+
+        modelBuilder.Entity<EventType>(entity =>
+        {
+            entity.HasKey(e => e.EventTypeId).HasName("PK__EventTyp__04ACC49DA227BC86");
+
+            entity.ToTable("EventType");
+
+            entity.HasIndex(e => e.EventTypeName, "UQ__EventTyp__F1C27EB1ECD28DB5").IsUnique();
+
+            entity.Property(e => e.EventTypeId).HasColumnName("eventTypeID");
+            entity.Property(e => e.EventTypeName)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("eventTypeName");
+        });
+
+        modelBuilder.Entity<Order>(entity =>
+        {
+            entity.HasKey(e => e.OrderId).HasName("PK__Orders__0809337D76E9BEFE");
+
+            entity.Property(e => e.OrderId).HasColumnName("orderID");
+            entity.Property(e => e.CustomerId).HasColumnName("customerID");
+            entity.Property(e => e.NumberOfTickets).HasColumnName("numberOfTickets");
+            entity.Property(e => e.OrderedAt)
+                .HasColumnType("datetime")
+                .HasColumnName("orderedAt");
+            entity.Property(e => e.TicketCategoryId).HasColumnName("ticketCategoryID");
+            entity.Property(e => e.TotalPrice)
+                .HasColumnType("numeric(10, 2)")
+                .HasColumnName("totalPrice");
+
+            entity.HasOne(d => d.Customer).WithMany(p => p.Orders)
+                .HasForeignKey(d => d.CustomerId)
+                .HasConstraintName("FK__Orders__customer__66361833");
+
+            entity.HasOne(d => d.TicketCategory).WithMany(p => p.Orders)
+                .HasForeignKey(d => d.TicketCategoryId)
+                .HasConstraintName("FK__Orders__ticketCa__672A3C6C");
+        });
+
+        modelBuilder.Entity<TicketCategory>(entity =>
+        {
+            entity.HasKey(e => e.TicketCategoryId).HasName("PK__TicketCa__56F2E67AFB010EC4");
+
+            entity.ToTable("TicketCategory");
+
+            entity.Property(e => e.TicketCategoryId).HasColumnName("ticketCategoryID");
+            entity.Property(e => e.Description)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("description");
+            entity.Property(e => e.EventId).HasColumnName("eventID");
+            entity.Property(e => e.Price)
+                .HasColumnType("numeric(10, 2)")
+                .HasColumnName("price");
+
+            entity.HasOne(d => d.Event).WithMany(p => p.TicketCategories)
+                .HasForeignKey(d => d.EventId)
+                .HasConstraintName("FK__TicketCat__event__6359AB88");
+        });
+
+        modelBuilder.Entity<TotalNumberOfTicketsPerCategory>(entity =>
+        {
+            entity
+                .HasNoKey()
+                .ToView("total_number_of_tickets_per_category");
+
+            entity.Property(e => e.FinalTotalPrice)
+                .HasColumnType("numeric(38, 2)")
+                .HasColumnName("finalTotalPrice");
+            entity.Property(e => e.TicketCategoryId).HasColumnName("ticketCategoryID");
+            entity.Property(e => e.TotalNrOfTickets).HasColumnName("totalNrOfTickets");
+        });
+
+        modelBuilder.Entity<Venue>(entity =>
+        {
+            entity.HasKey(e => e.VenueId).HasName("PK__Venue__4DDFB71FEBA05BDC");
+
+            entity.ToTable("Venue");
+
+            entity.Property(e => e.VenueId).HasColumnName("venueID");
+            entity.Property(e => e.Capacity).HasColumnName("capacity");
+            entity.Property(e => e.Location)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("location");
+            entity.Property(e => e.Type)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("type");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
