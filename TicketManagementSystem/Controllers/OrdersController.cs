459c9e69799689ff5db8959fc735784a6d71e584
Lucian B <lucian.bratu@stud.ubbcluj.ro>
2023-07-27 12:46:41 +0300

    Add information to the log for each endpoint

--- TicketManagementSystem/Controllers/OrdersController.cs
+++ TicketManagementSystem/Controllers/OrdersController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TicketManagementSystem.Middleware;
 using TicketManagementSystem.Models;
 using TicketManagementSystem.Models.DTOs;
 using TicketManagementSystem.Services;
@@ -11,20 +12,24 @@ namespace TicketManagementSystem.Controllers
     public class OrdersController : ControllerBase
     {
         private IOrdersService _ordersService;
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
 
-        public OrdersController(IOrdersService ordersService)
+        public OrdersController(IOrdersService ordersService, ILogger<ExceptionHandlingMiddleware> logger)
         {
             _ordersService = ordersService;
+            _logger = logger;
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Order>>> GetAll() {
+            _logger.LogInformation("Get Request method GetAll() called");
             return Ok(await this._ordersService.GetOrders());
         }
 
         [HttpGet]
         public async Task<ActionResult<Order>> GetById(int id)
         {
+            _logger.LogInformation("Get Request method GetById() called");
             OrdersDTO order =  await this._ordersService.GetOrderById(id);
             if(order == null)
             {
@@ -37,6 +42,7 @@ namespace TicketManagementSystem.Controllers
         [Route("{id:long}")]
         public async Task<ActionResult<OrdersDTO>> DeleteOrder([FromRoute] long id)
         {
+            _logger.LogInformation(FormattableString.Invariant($"Delete Request method DeleteOrder called with id: '{id}'"));
             OrdersDTO order = await this._ordersService.RemoveOrder(id);
             if( order == null )
             {
@@ -49,6 +55,7 @@ namespace TicketManagementSystem.Controllers
         [Route("{id:long}")]
         public async Task<ActionResult<OrdersUpdateDTO>> UpdateOrder([FromRoute] long id, OrdersUpdateDTO ordersUpdateDTO)
         {
+            _logger.LogInformation(FormattableString.Invariant($"Patch Request method UpdateOrder called with id: '{id}' and OrdersUpdateDTO: orderedAt: {ordersUpdateDTO.OrderedAt}, numberOfTickets: {ordersUpdateDTO.NumberOfTickets}"));
             OrdersUpdateDTO order = await this._ordersService.UpdateOrder(id, ordersUpdateDTO);
             if (order == null)
             {
--- TicketManagementSystem/Middleware/ExceptionHandlingMiddleware.cs
+++ TicketManagementSystem/Middleware/ExceptionHandlingMiddleware.cs
@@ -14,6 +14,7 @@ namespace TicketManagementSystem.Middleware
             _nextRequestDelegate = nextRequestDelegate;
             _logger = logger;
         }
+
         public async Task InvokeAsync(HttpContext httpContext)
         {
             try
